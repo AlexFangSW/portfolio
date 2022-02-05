4948645c3e463fb1f5a56679b310b9d0816bc0a5
AlexFangSW <alexfangsw@gmail.com>
2022-02-05 15:27:26 +0800

    background

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
